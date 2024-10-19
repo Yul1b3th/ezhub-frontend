1da01cc7cba269db3523ef66ef17ac91f216e5a6
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-10-19 10:34:07 +0200

    chore: update Prettier to the latest version

--- src/app/services/public-property.service.ts
+++ src/app/services/public-property.service.ts
@@ -1,4 +1,11 @@
-import { EnvironmentInjector, Injectable, computed, inject, runInInjectionContext, signal } from '@angular/core';
+import {
+  EnvironmentInjector,
+  Injectable,
+  computed,
+  inject,
+  runInInjectionContext,
+  signal,
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -33,24 +40,26 @@ export class PublicPropertyService {
   }
 
   getPublicPropertiesSignals(): void {
-    this.fetchProperties().subscribe(properties => {
+    this.fetchProperties().subscribe((properties) => {
       this.updateState(properties);
     });
   }
 
-private fetchProperties(): Observable<Property[]> {
-  return this.http
-    .get<Property[]>(`${this.baseUrl}/public-properties`)
-    .pipe(
+  private fetchProperties(): Observable<Property[]> {
+    console.log(this.baseUrl);
+
+    return this.http.get<Property[]>(`${this.baseUrl}/public-properties`).pipe(
       // tap(properties => console.log('Response:', properties)),
       map(this.filterAvailableProperties.bind(this)),
-      tap(properties => this.updateState(properties)),
+      tap((properties) => this.updateState(properties)),
       catchError(this.handleError.bind(this))
     );
-}
+  }
 
   private filterAvailableProperties(properties: Property[]): Property[] {
-    return properties.filter(property => property.deletedAt == null && property.is_available);
+    return properties.filter(
+      (property) => property.deletedAt == null && property.is_available
+    );
   }
 
   private updateState(properties: Property[]): void {
@@ -63,22 +72,24 @@ private fetchProperties(): Observable<Property[]> {
 
   private handleError(error: any): Observable<Property[]> {
     this.notificationService.showNotification(
-      'Unable to fetch available properties at this time. Please try again later.', 'error'
+      'Unable to fetch available properties at this time. Please try again later.',
+      'error'
     );
     console.error('Error fetching available properties:', error);
     return of([]);
   }
 
-getPropertyById(id: number): Observable<Property | null> {
-  return this.http.get<Property>(`${this.baseUrl}/public-properties/${id}`).pipe(
-    catchError(error => {
-      this.notificationService.showNotification(
-        `Error fetching property by ID ${id}: ${error.message}`, 'error'
+  getPropertyById(id: number): Observable<Property | null> {
+    return this.http
+      .get<Property>(`${this.baseUrl}/public-properties/${id}`)
+      .pipe(
+        catchError((error) => {
+          this.notificationService.showNotification(
+            `Error fetching property by ID ${id}: ${error.message}`,
+            'error'
+          );
+          return of(null); // Retorna null para indicar que la propiedad no fue encontrada
+        })
       );
-      return of(null); // Retorna null para indicar que la propiedad no fue encontrada
-    })
-  );
-}
-
-
+  }
 }
