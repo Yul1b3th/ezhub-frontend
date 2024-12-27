3b4aed898b1510fd2fad4907ca0e37dbe7bf4d09
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-12-27 13:01:37 +0100

    Update variables, mixins, and functions for design system

--- src/app/design-system/components/ds-button/ds-button.component.ts
+++ src/app/design-system/components/ds-button/ds-button.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Input,
+} from '@angular/core';
+
+import { ThemeService } from '@design-system/services/theme.service';
 
 @Component({
-  selector: 'app-ds-button',
-  imports: [],
+  selector: 'ds-button',
+  imports: [CommonModule],
   templateUrl: './ds-button.component.html',
-  styleUrl: './ds-button.component.scss'
+  styleUrl: './ds-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DsButtonComponent {
+  private themeService = inject(ThemeService);
 
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() btnClass: string = 'btn-primary';
+  @Input() size: 'btn-sm' | 'btn-md' | 'btn-lg' = 'btn-md';
+  @Input() block: boolean = false;
+  @Input() outline: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() ariaLabel: string = '';
+
+  get buttonClasses(): string[] {
+    const themeClass = this.themeService.theme();
+    const baseClass = this.outline
+      ? `btn-outline-${this.btnClass.replace('btn-', '')}`
+      : this.btnClass;
+    return [
+      'btn',
+      baseClass,
+      this.size,
+      themeClass,
+      this.block ? 'btn-block' : '',
+    ].filter(Boolean);
+  }
 }
--- src/app/design-system/pages/button/button.component.ts
+++ src/app/design-system/pages/button/button.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { DsButtonComponent } from '@design-system/components/ds-button/ds-button.component';
 
 @Component({
-  selector: 'button',
-  imports: [],
+  selector: 'page-button',
+  imports: [DsButtonComponent],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ButtonComponent {}
